652c1904ccdb6ec3d01c123b2de5f3824ed1ae4a
Alfonso Jimenez <djalfo18@gmail.com>
2024-06-02 22:07:55 -0500

    Importing custom components into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AboutSection from "@/components/AboutSection";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import PortfolioSection from "@/components/PortfolioSection";
+import ProjectSection from "@/components/ProjectSection";
+import SkillSection from "@/components/SkillSection";
+import SocialSection from "@/components/SocialSection";
 
 export default function Home() {
   return (
     <div>
-      <h1>This is my portfolio</h1>
+      <Navbar />
+      <PortfolioSection />
+      <AboutSection />
+      <ProjectSection />
+      <SkillSection />
+      <Footer />
+      <SocialSection />
     </div>
   );
 }

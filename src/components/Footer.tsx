c42aa4af75df6f58a2ccd0e056aa5344bec5cc9d
Alfonso Jimenez <djalfo18@gmail.com>
2024-06-04 15:43:23 -0500

    Adding texts, contact e-mail and styles into Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import styles from '@/styles/Footer.module.css';
 
 const Footer = () => {
     return (
-        <div></div>
+        <div className={styles.section4outermost}>
+            <div className={styles.left}>
+                <p>If you are Looking for a web/front-end developer or need
+                help with your project then feel free to contact me at
+                <span><a href="mailto: djalfo18@gmail.com">djalfo18@gmail.com</a></span>
+                </p>
+            </div>
+            <div className={styles.right}>
+                <h1>
+                Let&apos;s<br />
+                Work<br />
+                together
+                </h1>
+            </div>
+        </div>
     )
 }
 

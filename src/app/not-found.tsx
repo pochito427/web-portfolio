d74a7fb946179c2f84b5f0be7aa66d9b6f07e4aa
Alfonso Jimenez <djalfo18@gmail.com>
2024-07-28 11:26:25 -0500

    Fix no unescaped entities typos for Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <html>
       <body>
         <h1>Page Not Found</h1>
-        <p>Oops! It seems like you've stumbled upon a page that doesn't exist. Don't worry, our internationalization magic is still hard at work on other parts of the app. Feel free to navigate back to explore more!</p>
+        <p>Oops! It seems like you have stumbled upon a page that does not exist. Do not worry, our internationalization magic is still hard at work on other parts of the app. Feel free to navigate back to explore more!</p>
         <Link href="/">Return Home</Link>
       </body>
     </html>
